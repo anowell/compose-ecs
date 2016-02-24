b5fc4bdbe00342a2c345df64a9f5ac8f22847593
Louis McCormack <louismccormack@hotmail.co.uk>
2016-02-24 18:05:56 +0000

    rspec tests for asg check

--- spec/uploader/uploader_spec.rb
+++ spec/uploader/uploader_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
+require 'ostruct'
 
 class MockAWS 
   class Cloudformation 
+
+    def stacks
+      { "mystack" =>
+      OpenStruct.new(:resources => [ OpenStruct.new(:resource_type => "AWS::AutoScaling::AutoScalingGroup", 
+                                                  :physical_resource_id => "my_as_group", 
+                                                  :logical_resource_id => "my_logical_resource" 
+                                                  ) ]
+      )
+      }
+    end
+
     def create_stack(opts) 
       return true 
     end
@@ -10,6 +22,13 @@ class MockAWS
       return true
     end
   end
+
+  class AutoScaling
+    def groups
+      { "my_as_group" => OpenStruct.new(:desired_capacity => 10) }
+    end
+  end
+
 end
 
 include Spaceape::AWS
@@ -31,7 +50,7 @@ describe Spaceape::Cloudformation::Uploader do
     end
 
     it 'should initialize itself with a different region if provided' do
-      @uploader = Spaceape::Cloudformation::EcsGenerator.new("ecs", "myService", "myRegion")
+      @uploader = Spaceape::Cloudformation::Uploader.new("myService", "myEnv", "myRegion")
       @uploader.region.equal?("myRegion")
     end
   end
@@ -49,8 +68,50 @@ describe Spaceape::Cloudformation::Uploader do
     it "should update a stack with the correct policy" do
       @cf = MockAWS::Cloudformation.new
       allow_any_instance_of(Spaceape::AWS).to receive(:setup_amazon).with('CloudFormation::Client', '~/.aws/config', 'us-east-1').and_return(@cf)
-      expect(@cf).to receive(:update_stack).with({:stack_name=>nil, :template_body=>"{}\n", :capabilities=>["CAPABILITY_IAM"], :stack_policy_during_update_body=>"{ \"TEST\" : 1 }\n"})
-      @uploader.update_stack({:policy => File.expand_path('../../mock_skels/myPolicy.json', __FILE__)})
+      expect(@uploader).to receive(:check_asg_size).with("mystack", "myService/myEnv/myService.json").and_return(true)
+      expect(@cf).to receive(:update_stack).with({:stack_name=>"mystack", :template_body=>"{}\n", :capabilities=>["CAPABILITY_IAM"], :stack_policy_during_update_body=>"{ \"TEST\" : 1 }\n"})
+      @uploader.update_stack({:stackname => "mystack", :policy => File.expand_path('../../mock_skels/myPolicy.json', __FILE__)})
+    end
+
+    it 'should sanity check the ASG size(s)' do
+      @tmpl=<<JSON
+{
+   "AWSTemplateFormatVersion" : "2010-09-09",
+   "Resources" : {
+      "my_logical_resource" : {
+         "Properties" : {
+            "MaxSize" : {
+               "Ref" : "MaxInstances"
+            },
+            "MinSize" : {
+               "Ref" : "MinInstances"
+            }
+         }
+      }
+   },
+   "Description" : "testy testy",
+   "Parameters" : {
+      "MaxInstances" : {
+         "Type" : "Number",
+         "Default" : 1,
+         "Description" : "Maximum instances in AutoScale group"
+      }
+   }
+}
+JSON
+      File.open('myService/myEnv/myService.json', 'w') {|f| f.write(@tmpl) }
+      @cf = MockAWS::Cloudformation.new
+      @as = MockAWS::AutoScaling.new
+      allow(@uploader).to receive(:setup_amazon).once.ordered.with('AutoScaling', '~/.aws/config', 'us-east-1').and_return(@as)
+      allow(@uploader).to receive(:setup_amazon).once.ordered.with('CloudFormation', '~/.aws/config', 'us-east-1').and_return(@cf)
+      lambda { @uploader.update_stack({:stackname => "mystack", :policy => File.expand_path('../../mock_skels/myPolicy.json', __FILE__)}) }.should raise_error SystemExit 
+    end
+
+    it "should not sanity check the ASG size if :no_asg_check is passed" do
+      @cf = MockAWS::Cloudformation.new
+      allow_any_instance_of(Spaceape::AWS).to receive(:setup_amazon).with('CloudFormation::Client', '~/.aws/config', 'us-east-1').and_return(@cf)
+      expect(@cf).to receive(:update_stack).with({:stack_name=>"mystack", :template_body=>"{}\n", :capabilities=>["CAPABILITY_IAM"], :stack_policy_during_update_body=>"{ \"TEST\" : 1 }\n"})
+      @uploader.update_stack({:stackname => "mystack", :policy => File.expand_path('../../mock_skels/myPolicy.json', __FILE__), :no_asg_check => true})
     end
   end
 
